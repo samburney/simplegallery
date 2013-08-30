4357c50038129e03fad22f2e30bba00bfe799f7f
Sam Burney <sam.burney@air-stream.org>
2013-08-30 13:34:53 +0930

    * Updated form styles

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -7,7 +7,7 @@
 @section ('content')
 <div class="row">
 	<h2 class="text-center">User Login</h2>
-	<div class="col-offset-2 col-lg-8 well">
+	<div class="col-lg-offset-2 col-lg-8 col-sm-offset-2 col-sm-8 well">
 		{{Former::framework('Nude')}}
 		{{Former::open()
 			->class('form-horizontal')
@@ -17,14 +17,14 @@
 			))
 		}}
 		<div class="form-group">
-			{{Former::label('Username or Email', 'username')->class('col-lg-4 control-label')}}
-			<div class="col-lg-8">
+			{{Former::label('Username or Email', 'username')->class('col-lg-4 col-sm-4 control-label')}}
+			<div class="col-lg-8 col-sm-8">
 				{{Former::text('username')->class('form-control')->label(false)}}
 			</div>
 		</div>
 		<div class="form-group">
-			{{Former::label('Password', 'password')->class('col-lg-4 control-label')}}
-			<div class="col-lg-8">
+			{{Former::label('Password', 'password')->class('col-lg-4 col-sm-4 control-label')}}
+			<div class="col-lg-8 col-sm-8">
 				{{Former::password('password')->class('form-control')->label(false)}}
 				<div class="checkbox">
 					{{Former::checkbox('remember', false)}}
--- app/views/users/register.blade.php
+++ app/views/users/register.blade.php
@@ -5,9 +5,9 @@
 @endsection
 
 @section ('content')
-<div class="row-fluid">
+<div class="row">
 	<h2 class="text-center">User Registration</h2>
-	<div class="col-offset-2 col-lg-8 well">
+	<div class="col-lg-offset-2 col-lg-8 col-sm-offset-2 col-sm-8 well">
 		{{Former::framework('Nude')}}
 		{{Former::open()
 			->rules(array(
@@ -19,26 +19,26 @@
 			->class('form-horizontal')
 		}}
 		<div class="form-group">
-			{{Former::label('Username', 'username')->class('col-lg-4 control-label')}}
-			<div class="col-lg-8">
+			{{Former::label('Username', 'username')->class('col-lg-4 col-sm-4 control-label')}}
+			<div class="col-lg-8 col-sm-8">
 				{{Former::text('username')->class('form-control')->label(false)}}
 			</div>
 		</div>
 		<div class="form-group">
-			{{Former::label('Email', 'email')->class('col-lg-4 control-label')}}
-			<div class="col-lg-8">
+			{{Former::label('Email', 'email')->class('col-lg-4 col-sm-4 control-label')}}
+			<div class="col-lg-8 col-sm-8">
 				{{Former::text('email')->class('form-control')->label(false)}}
 			</div>
 		</div>
 		<div class="form-group">
-			{{Former::label('Password', 'password')->class('col-lg-4 control-label')}}
-			<div class="col-lg-8">
+			{{Former::label('Password', 'password')->class('col-lg-4 col-sm-4 control-label')}}
+			<div class="col-lg-8 col-sm-8">
 				{{Former::password('password')->class('form-control')->label(false)}}
 			</div>
 		</div>
 		<div class="form-group">
-			{{Former::label('Confirm Password', 'password')->class('col-lg-4 control-label')}}
-			<div class="col-lg-8">
+			{{Former::label('Confirm Password', 'password')->class('col-lg-4 col-sm-4 control-label')}}
+			<div class="col-lg-8 col-sm-8">
 				{{Former::password('password_confirm')->class('form-control')->label(false)}}
 			</div>
 		</div>
